bbf316203987495d5feb25ad915af8083768fd48
Pavl0ffN <pavl0ffn94@gmail.com>
2023-02-18 20:01:00 +0400

    modal add location storage

--- Простое добавление задач/app.js
+++ Простое добавление задач/app.js
@@ -1,14 +1,28 @@
 const list = document.getElementById("todos");
 document.querySelector("button").addEventListener("click", handleClick);
+document.addEventListener("DOMContentLoaded", loadTodos);
+
 function handleClick() {
   const newTodo = this.previousElementSibling.value.trim();
   if (newTodo) {
     createTodo(newTodo);
+    saveToStorage(newTodo);
     this.previousElementSibling.value = "";
   } else {
     alert("input field is empty");
   }
 }
+function saveToStorage(todo) {
+  const todos = JSON.parse(localStorage.getItem("tasks")) || [];
+  localStorage.setItem("tasks", JSON.stringify([...todos, todo]));
+}
+function loadTodos() {
+  const todos = JSON.parse(localStorage.getItem("tasks"));
+  if (todos) {
+    todos.forEach((todo) => createTodo(todo));
+  }
+}
+
 function createTodo(text) {
   const li = document.createElement("li");
   li.innerText = text;
