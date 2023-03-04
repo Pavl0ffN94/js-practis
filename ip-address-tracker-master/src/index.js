3e65efed5351b78fad8613049b0d513d4649503e
Pavl0ffN <pavl0ffn94@gmail.com>
2023-03-04 23:33:41 +0400

    step 2

--- ip-address-tracker-master/src/index.js
+++ ip-address-tracker-master/src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+import { validateIp } from "./helpers";
+
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+function getData() {
+  if (validateIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_hg128EJnaxGK8Aj0MU39AD15yDsNY&ipAddress=${ipInput.value}`,
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
