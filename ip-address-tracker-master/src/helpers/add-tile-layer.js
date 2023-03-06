e37c62d834422ac950529bf4dd805bfd38c86547
Pavl0ffN <pavl0ffn94@gmail.com>
2023-03-06 23:14:49 +0400

    step 3

---
+++ ip-address-tracker-master/src/helpers/add-tile-layer.js
@@ -0,0 +1,15 @@
+import L from "leaflet";
+
+export function addTileLayer(map) {
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGFsMGZmbiIsImEiOiJjbGV4MHk1bmswaTd3M3Jxd3Ryczlja2dqIn0.2k-r2w-60WJcjcm7NWoySA",
+    {
+      attribution:
+        'Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="#">Pavlov Nikolai</a>.',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+    },
+  ).addTo(map);
+}
--- ip-address-tracker-master/src/index.js
+++ ip-address-tracker-master/src/index.js
@@ -1,16 +1,38 @@
-import { validateIp } from "./helpers";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import {addTileLayer, validateIp} from "./helpers";
+import icon from "../images/icon-location.svg";
 
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector("button");
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timeZoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+});
+
+const mapArea = document.querySelector(".map");
+const map = L.map(mapArea, {
+  center: [51.505, -0.09],
+  zoom: 13,
+});
+addTileLayer(map);
+L.marker([51.505, -0.09], {icon: markerIcon}).addTo(map);
+
 function getData() {
   if (validateIp(ipInput.value)) {
     fetch(
       `https://geo.ipify.org/api/v2/country?apiKey=at_hg128EJnaxGK8Aj0MU39AD15yDsNY&ipAddress=${ipInput.value}`,
     )
-      .then((response) => response.json())
-      .then(console.log);
+      .then(response => response.json())
+      .then(setInfo);
   }
 }
 
@@ -18,4 +40,12 @@ function handleKey(e) {
   if (e.key === "Enter") {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  console.log(mapData);
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = mapData.location.country + " " + mapData.location.region;
+  timeZoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
